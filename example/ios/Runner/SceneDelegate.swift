63c9970f9a4c40b9ab52a4672e8fc6b9738ea55f
Enguerrand ARMINJON <37028599+EArminjon@users.noreply.github.com>
2025-09-10 16:10:19 +0200

    feat: Update example folder and doc (#70)
    * feat: Update example folder and doc
    
    * Update doc

--- example/ios/Runner/SceneDelegate.swift
+++ example/ios/Runner/SceneDelegate.swift
@@ -7,19 +7,16 @@
 
 @available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    var window: UIWindow?
+  var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = scene as? UIWindowScene else { return }
 
-        guard let windowScene = scene as? UIWindowScene else { return }
+    window = UIWindow(windowScene: windowScene)
 
-        window = UIWindow(windowScene: windowScene)
-        let flutterEngine = FlutterEngine(name: "SceneDelegateEngine")
-        flutterEngine.run()
-        GeneratedPluginRegistrant.register(with: flutterEngine)
-        let controller = FlutterViewController.init(engine: flutterEngine, nibName: nil, bundle: nil)
-        controller.loadDefaultSplashScreenView()
-        window?.rootViewController = controller
-        window?.makeKeyAndVisible()
-    }
+    let controller = FlutterViewController.init(engine: flutterEngine, nibName: nil, bundle: nil)
+    controller.loadDefaultSplashScreenView()
+    window?.rootViewController = controller
+    window?.makeKeyAndVisible()
+  }
 }
